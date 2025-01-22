fdfdb4fe5806d1ec12cb8d8d9d2976b2bbd2d315
sumr <simarjeet.sm11@gmail.com>
2025-01-22 18:18:58 +0000

    change styling of the shadcn button component

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+
+export default function Home() {
+  return (
+    <div className="flex  gap-2">
+      <Button variant={"primary"} disabled = {true}>Primary</Button>
+      <Button variant={"destructive"}>Destructive</Button>
+      <Button variant={"ghost"}>Ghost</Button>
+      <Button variant={"muted"}>Muted</Button>
+      <Button variant={"outline"}>Outline</Button>
+      <Button variant={"secondary"}>Secondary</Button>
+      <Button variant={"teritrary"}>teritrary</Button>
+
+
+
+      <Input/>
+
+
+    </div>
+  )
+}
